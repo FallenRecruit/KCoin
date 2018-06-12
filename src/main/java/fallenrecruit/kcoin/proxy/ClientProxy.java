fd498220234df8dfe6ca4ac31754634a07cd9d0c
FallenRecruit <pufferfish0101@gmail.com>
2018-06-13 03:27:18 +1000

    Initial

---
+++ src/main/java/fallenrecruit/kcoin/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package fallenrecruit.kcoin.proxy;
+
+public class ClientProxy  extends CommonProxy {
+
+}
---
+++ src/main/java/fallenrecruit/kcoin/util/Reference.java
@@ -0,0 +1,12 @@
+package fallenrecruit.kcoin.util;
+
+public class Reference {
+
+	 public static final String MOD_ID = "kcoin";
+	 public static final String NAME = "k coin";
+	 public static final String VERSION = "1.0";
+	 public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	 public static final String CLIENT_PROXY_CLASS = "fallenrecruit.kcoin.proxy.ClientProxy";
+	 public static final String COMMON_PROXY_CLASS = "fallenrecruit.kcoin.proxy.CommonProxy";
+	 
+}
